0b8a5a0cbec241825285717dc736777e6a35cfe4
Yasin Khan <yasinkhan500@gmail.com>
2022-01-12 16:20:45 -0800

    ELT done

---
+++ db/database.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE SDC;
+
+DROP TABLE IF EXISTS reviews, photos, characteristics, characteristics_reviews;
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    product_id INT,
+    rating INT,
+    date BIGINT,
+    summary VARCHAR (500),
+    body VARCHAR (500),
+    recommend BOOLEAN,
+    reported BOOLEAN,
+    reviewer_name VARCHAR (50),
+    reviewer_email VARCHAR (50),
+    response VARCHAR (500),
+    helpfulness INT DEFAULT 0
+);
+
+COPY reviews(id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) FROM '/private/tmp/test_reviews.csv' DELIMITER ',' CSV HEADER;
+CREATE TABLE photos (
+    id SERIAL PRIMARY KEY,
+    review_id INT,
+    url VARCHAR
+);
+
+COPY photos(id, review_id, url) FROM '/private/tmp/test_reviews_photos.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE characteristics (
+    id SERIAL PRIMARY KEY,
+    product_id INT,
+    name VARCHAR (50)
+);
+
+COPY characteristics(id, product_id, name) FROM '/private/tmp/test_characteristics.csv' DELIMITER ',' CSV HEADER;
+
+
+CREATE TABLE characteristics_reviews (
+    id SERIAL PRIMARY KEY,
+    characteristic_id INT,
+    review_id INT,
+    value INT
+);
+
+COPY characteristics_reviews(id, characteristic_id, review_id, value) FROM '/private/tmp/test_characteristics_reviews.csv' DELIMITER ',' CSV HEADER;
+
+
+
---
+++ db/index.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'password',
+  host: 'localhost',
+  port: 5432,
+  database: 'sdcRR',
+});
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+module.exports = pool;
